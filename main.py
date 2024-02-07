11817483bc88894089da91a2c1931255b9f2beb4
KumarGN69 <kumargn1969@hotmail.com>
2024-02-07 12:11:20 -0600

    first chat gpt AP call

---
+++ main.py
@@ -0,0 +1,22 @@
+from openai import OpenAI
+
+MY_API_KEY = "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"
+# openai.my_api_key = YOUR_API_KEY
+
+client = OpenAI(api_key=MY_API_KEY)
+# client.
+
+messages = [ {"role": "system", "content": "You are a intelligent assistant."} ]
+
+
+message = input("User : ")
+if message:
+    messages.append(
+        {"role": "user", "content": message},
+    )
+completion = client.chat.completions.create(
+        model="gpt-3.5-turbo", messages=messages
+    )
+reply = completion.choices[0].message.content
+print(f"ChatGPT: {reply}")
+# messages.append({"role": "assistant", "content": reply})
