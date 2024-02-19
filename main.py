38808ad1bf1d33e0a94ac6a1288a50b365ffb9f9
KumarGN69 <kumargn1969@hotmail.com>
2024-02-19 22:52:58 +0000

    Flask Tutorial

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello World')
+from flask import Flask, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    rand_num = random.randint(1,10)
+    return render_template("index.html", random_number=rand_num)
+
+if __name__ == "__main__":
+    app.run()
