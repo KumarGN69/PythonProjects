883002f1fd6082abb740ac928e300076143fd272
KumarGN69 <kumargn1969@hotmail.com>
2024-02-20 00:50:20 +0000

    Flask updates

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 import random
+from datetime import datetime
+
+current_year = datetime.today().year
 
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
     rand_num = random.randint(1,10)
-    return render_template("index.html", random_number=rand_num)
+    # current_year = datetime.today().year()
+    return render_template("index.html", random_number=rand_num, year=current_year)
 
 if __name__ == "__main__":
     app.run()
